02df7b57dacdd41ed384c514a45424547aaf9a84
gopalbasak1 <gopalbasak1223@gmail.com>
2024-07-16 12:36:20 +0600

    display number dynamically to javascricpt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import { getMovieReviewData } from "./data.js";
 
-console.log(getMovieReviewData());
+function init() {
+    const movieReviewData = getMovieReviewData();
+
+    paintStatistics(movieReviewData);
+}
+
+function paintStatistics(movieReviewData){
+    const flatReviewData = movieReviewData.flat();
+
+    const totalMovies = movieReviewData.length;
+    const totalReviews = flatReviewData.length;
+    const totalRating = flatReviewData.reduce((acc, item)=> {
+        return acc + item.rating;
+    },0);
+    const avgRating = (totalRating/totalReviews).toFixed(2);
+
+    const totalMoviesEl = document.getElementById("tMoviesId");
+    addStat(totalMoviesEl, totalMovies);
+
+    const avgRatingEl = document.getElementById("tAvgRatingId");
+    addStat(avgRatingEl, avgRating);
+
+    const totalReviewEl = document.getElementById("tReviewId");
+    addStat(totalReviewEl, totalReviews);
+    
+}
+
+function addStat(elem, value){
+    const spanEl = document.createElement("span");
+    spanEl.classList.add("text-6xl")
+    spanEl.innerText = value;
+    elem.appendChild(spanEl)
+}
+
+init()
