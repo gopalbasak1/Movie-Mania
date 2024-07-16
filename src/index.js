4d9c6d99603d53d1fb219dd1f0a0835133f0b98f
gopalbasak1 <gopalbasak1223@gmail.com>
2024-07-16 13:05:50 +0600

    review session dynamic data show

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ function init() {
     const movieReviewData = getMovieReviewData();
 
     paintStatistics(movieReviewData);
+    paintMovieData(movieReviewData);
 }
 
 function paintStatistics(movieReviewData){
     const flatReviewData = movieReviewData.flat();
+    //console.log(flatReviewData);
 
     const totalMovies = movieReviewData.length;
     const totalReviews = flatReviewData.length;
@@ -32,6 +34,39 @@ function addStat(elem, value){
     spanEl.classList.add("text-6xl")
     spanEl.innerText = value;
     elem.appendChild(spanEl)
+};
+
+
+function paintMovieData(movieReviewData){
+    const flatReviewData = movieReviewData.flat();
+    const movieListEl = document.querySelector('#movieListId UL');
+
+    console.log(movieListEl);
+    
+    flatReviewData.map((movie)=>{
+        console.log(movie);
+    
+        const liElem = document.createElement('li');
+        liElem.classList.add("card", "p-2", "my-2")
+
+        const titleElem = document.createElement('p');
+        titleElem.classList.add('text-xl', 'mb-2');
+        titleElem.innerText = `${movie.title} - ${movie.rating}`;
+        liElem.appendChild(titleElem);
+
+        const reviewElem = document.createElement('p');
+        reviewElem.classList.add('mx-2', 'mb-2');
+        reviewElem.innerText = movie.content
+        liElem.appendChild(reviewElem);
+
+        const byElem = document.createElement('p');
+        byElem.classList.add('mx-2', 'mb-2');
+        byElem.innerText = `By ${movie.by} - ${new Intl.DateTimeFormat('en-BD').format(movie.on)}`;
+        liElem.appendChild(byElem);
+
+        movieListEl.appendChild(liElem);
+
+    })
 }
 
-init()
+init();
